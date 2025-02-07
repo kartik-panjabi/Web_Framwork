aff9a7a1631b779ff16badc46a3fe89abd6ceda0
kartik-panjabi <82868772+kartik-panjabi@users.noreply.github.com>
2025-02-07 13:53:03 -0500

    navlink updated

--- N01600598_DashBoard_App/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ N01600598_DashBoard_App/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -12,6 +12,7 @@ import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
+//new comment
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -39,6 +40,7 @@ export default function NavLinks() {
               },
             )}
           >
+            
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </a>
