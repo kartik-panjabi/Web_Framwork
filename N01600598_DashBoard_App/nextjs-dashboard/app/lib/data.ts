fa4b31bb4377dbb8b092c64d2fce429d757eec89
kartik-panjabi <82868772+kartik-panjabi@users.noreply.github.com>
2025-02-09 14:09:00 -0500

    dashboard card added 1

--- N01600598_DashBoard_App/nextjs-dashboard/app/lib/data.ts
+++ N01600598_DashBoard_App/nextjs-dashboard/app/lib/data.ts
@@ -75,6 +75,8 @@ export async function fetchCardData() {
 
     const numberOfInvoices = Number(data[0][0].count ?? '0');
 const numberOfCustomers = Number(data[1][0].count ?? '0');
+
+
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
 
